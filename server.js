293704c46c64177cb34e0a499838a6ddca1586a9
Carolyn Remmler <carolynremmler@C-L-MAC-4279.local>
2017-05-28 00:39:11 -0500

    zip up and download

--- server.js
+++ server.js
@@ -9,24 +9,9 @@ Promise.promisifyAll(fs);
 
 const PORT = process.env.PORT || 3000;
 
-
-
-// https://github.com/Stuk/jszip/issues/293
-/*zip
-.generateNodeStream({streamFiles:true})
-.on('end', function () {
-  console.log("zip stream ended.");
-})
-.pipe(fs.createWriteStream('out.zip'))
-.on('finish', function () {
-  console.log("out.zip written.");
-});*/
-
-
 app.use(express.static(__dirname));
   
 app.post('/fileupload',function(req,res){
-  console.log("fog fileupload");
    var form = new formidable.IncomingForm();
    form.uploadDir = __dirname + '/nlogo';
    form.parse(req, function(err, fields, files) {
@@ -35,58 +20,51 @@ app.post('/fileupload',function(req,res){
      fs.rename(file.path, form.uploadDir + "/" + filename);
      var newData;
      fs.readFileAsync(filename, "utf8").then(function(data) {
-        //  console.log(contents);
-          var array = data.toString().split("\n");
-          for(i in array) {
-             if (array[i] === "@#$#@#$#@") { 
-               console.log("SECTION");
-               newData = newData + "SECTION\n";
-             }
-          }
+        var array = data.toString().split("\n");
+        for(i in array) {
+           if (array[i] === "@#$#@#$#@") { 
+             console.log("SECTION");
+             newData = newData + "SECTION\n";
+           }
+        }
       }).then(function() {
-        fs.writeFile(form.uploadDir+"/out"+file.name, newData, function(err) {
-          if (err) throw err;
-          console.log('complete');
-        });
+        //fs.writeFile(form.uploadDir+"/out"+file.name, newData, function(err) {
+        //  if (err) throw err;
+        //  console.log('complete');
+        //});
+      
       }).then(function() {
-        res.send("200");
-        /*var zip = new JSZip();
+        //res.download("package.json");
+        var zip = new JSZip();
         console.log("make zip files");
         zip.file("hello.txt", "Hello World\n");
         zip.file("dog.txt", "hellow dog");
-        
-        zip.generateAsync({type:"blob"})
-        .then(function (blob) {
-          console.log("saved as hello.zip");
-            saveAs(blob, "hello.zip");
-        });*/
-        /*zip.generateNodeStream({type:'nodebuffer',streamFiles:true})
-        .pipe(fs.createWriteStream('out.zip'))
-        .on('finish', function () {
-            // JSZip generates a readable stream with a "end" event,
-            // but is piped here in a writable stream which emits a "finish" event.
-            console.log("out.zip written.");
-        });*/
-      }).catch(function(e) {
-        res.send("200");
-          console.error(e.stack);
+        fs.readFileAsync("pig.html", "utf8").then(function(data) {
+           zip.file("piggies.txt", data);
+        }).then(function() {
+          fs.readFileAsync("pig.html", "utf8").then(function(data) {
+             zip.file("piggies2.txt", data);
+          }).then(function() {
+            zip.generateNodeStream({type:'nodebuffer',streamFiles:true})
+            .pipe(fs.createWriteStream('out.zip'))
+            .on('finish', function () {
+                res.download("out.zip", function() {
+                  var FullPath= __dirname + '/out.zip';
+                  console.log(FullPath);
+                  fs.unlink(FullPath, function() {
+                    console.log(FullPath + " deleted");
+                  });
+                });
+            });
+          }).catch(function(e) {
+            res.sendfile('index.html');
+            console.error(e.stack);
+          });
+        });
       });
    });
-   
 });
-  
-app.get('/filedownload', function(req, res){
-  console.log("download file");
-  var file = __dirname + '/package.json';
-  res.download(file); // Set disposition and send it.
-//  res.sendfile('index.html');
-  var FullPath= __dirname + '/nlogo/package.json';
-  console.log(FullPath);
-  fs.unlink(FullPath, function() {
-    console.log(FullPath + " deleted");
-  });
-});
-  
+
 app.get('/', function(req, res){
 	res.sendfile('index.html');
 });
